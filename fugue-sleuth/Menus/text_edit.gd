fb1421117c6ce70fea38df44db7e87e644cb47fa
Bullfrog <thebullfrog2709@gmail.com>
2026-08-25 12:46:02 +0800

    Made a detector for when the words hit the end of page. Right now it deletes everything, no problem because we can just change the code to make a new page.

--- fugue-sleuth/Menus/text_edit.gd
+++ fugue-sleuth/Menus/text_edit.gd
@@ -1,6 +1,16 @@
 extends TextEdit
 
+var safe_text : String = ""
+
 func _input(event : InputEvent):
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode == KEY_BACKSPACE or event.keycode == KEY_DELETE or (event.ctrl_pressed and (event.keycode == KEY_X or event.keycode == KEY_Z or event.keycode == KEY_C)):
 			get_viewport().set_input_as_handled()
+	
+
+
+func _on_text_changed() -> void:
+	if get_visible_line_count() > 15:
+		text = safe_text
+	elif get_visible_line_count() < 14:
+		safe_text = text
