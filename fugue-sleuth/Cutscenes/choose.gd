30e8ce1ab46c68023769b6fab5434333c14a617f
Bullfrog <thebullfrog2709@gmail.com>
2026-08-30 05:43:43 +0800

    Finished the alien cutscenes

--- fugue-sleuth/Cutscenes/choose.gd
+++ fugue-sleuth/Cutscenes/choose.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,8 +20,8 @@ func _on_become_pressed() -> void:
 
 
 func _on_turn_in_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cutscenes/turn_in.tscn")
+	get_tree().change_scene_to_file("res://Cutscenes/turnin.tscn")
 
 
 func _on_give_up_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cutscenes/give_up.tscn")
+	get_tree().change_scene_to_file("res://Cutscenes/giveup.tscn")
